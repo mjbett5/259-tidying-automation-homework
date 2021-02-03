e466b8a6f28d6e5b5f9ca3a481c242811ac01a97
mjbett5 <mbett007@ucr.edu>
2021-02-03 20:26:29 +0000

    double checking the commit went though

--- tidying_automation_homework.R
+++ tidying_automation_homework.R
@@ -44,17 +44,35 @@ tidying_automation_homework.R
 #Use a for loop with paths to read the data in to a new tibble "ds_loop" so that the data are combined into a single dataset
 #(Yes, Vroom does this automatically but practice doing it with a loop)
 #If you did this correctly, it should look the same as ds_combined created above
+new_data<-tibble()
+
+read_csv()
+for (file in paths){
+  temp_data<-read_csv(file)
+  
+  temp_data$file<- file
+  new_data<- bind_rows(new_data,temp_data)
+}
+
 
 ### Question 3 ----------
 
 #Use map with paths to read in the data to a single tibble called ds_map
 #If you did this correctly, it should look the same as ds_combined created above
+mapped_data<-map(paths,~ read_csv(.x))
+
+mapped_data<-bind_rows(mapped_data)
+
+
 
 ### Question 4 ----------
 
 #The data are in a wider-than-ideal format. 
 #Use pivot_longer to reshape the data so that sex is a column with values male/female and words is a column
 #Use ds_combined or one of the ones you created in Question 2 or 3, and save the output to ds_longer
+ds_longer<-pivot_longer(mapped_data,Female:Male,names_to ="Sex" ,values_to = "Count")
+
+
 
 ### Question 5 ----------
 
@@ -62,13 +80,17 @@ tidying_automation_homework.R
 #The tibble below contains the total number of words in each book (make sure to run those lines so that it appears in your environment)
 #Merge it into ds_longer and then create a new column that expresses the words spoken as a percentage of the total
 total_words <- tibble(Film =  c("The Fellowship Of The Ring", "The Two Towers","The Return Of The King"),
-                      Total = c(177277, 143436, 134462))
+                      Words = c(177277, 143436, 134462))
+
+ds_with_words_col<-left_join(ds_longer,total_words)
+
 
 ### Question 6 ----------
 #The function below creates a graph to compare the words spoken by race/sex for a single film
 #The input for the function is a tibble that contains only a single film
 #Write a for loop that iterates through the film names to apply the function to a subset of ds_longer (each film)
 #Run all 6 lines code below to define the function (it should show in your environment after running)
+
 words_graph <- function(df) {
   p <- ggplot(df, aes(x = Race, y = Words, fill = Sex)) + 
     geom_bar(stat = "identity", position = "dodge") + 
@@ -76,10 +98,17 @@ words_graph <- function(df) {
   print(p)
 }
 
+for (movie in unique(ds_with_words_col$Film)) {
+  words_graph(ds_with_words_col%>%filter(Film==movie))
+}
+
+
 ### Question 7 ----------
 
 #Apply the words_graph function again, but this time
 #use split and map to apply the function to each film separately
+
+
 
 ### Question 8 ---------- 
 
@@ -87,6 +116,9 @@ words_graph <- function(df) {
 #and separate columns for the words spoken by each race and the percentage of words spoken by each race
 #First, get the data formatted in the correct way
 #From ds_longer, create a new tibble "ds_wider" that has columns for words for each race and percentage for each race
+
+
+
 
 ### Question 9 ---------
 
